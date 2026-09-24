d05c516423155e616c1069690d3e3dc8b5ffd573
albiansyah2022-create <albiansyah2022@gmail.com>
2026-09-24 21:35:06 +0700

    save pertama

---
+++ pertemuan-1.py
@@ -0,0 +1,9 @@
+nama = input ("masukan nama anda")
+tahun_lahir = int(input("masukan tahun lahir anda"))
+tahun_skr = int(input("masukan tahun sekarang"))
+
+print("====================================================")
+print("nama anda adalah",nama)
+umur = tahun_skr - tahun_lahir
+print("umur anda adalah",umur)
+print("====================================================")
